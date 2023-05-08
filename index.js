32e7c3bb50fabc3781d276311253525a5b4c746a
arthasasta <116079643+arthasasta@users.noreply.github.com>
2023-05-08 20:24:44 +0700

    index.js

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const PORT = process.env.PORT || 5000;
 
 const multer = require('multer')
 const path = require('path')
+var cors = require('cors');
 
 // set body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({
+    origin: '*'  
+}))
 
 // script upload
 
